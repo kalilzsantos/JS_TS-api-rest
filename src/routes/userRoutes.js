aee8728ed4bceb9c42f9aa6fe18dbd9ea5ab8a78
kalilzsantos <kalilzsantos@gmail.com>
2022-06-21 11:29:52 -0300

    JWT verification middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController'; // letra minuscula pois o que vem é da clesse é um obj e não a classe
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.get('/', userController.index);
-router.get('/:id', userController.show);
 router.post('/', userController.create);
+router.get('/', loginRequired, userController.index);
+router.get('/:id', loginRequired, userController.show);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.delete);
 
